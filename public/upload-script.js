0d6210a7f2b019a1673640aa779250d78fbba769
ChenNan <fmd2024@163.com>
2024-01-24 16:04:16 +0800

    daily update

---
+++ public/upload-script.js
@@ -0,0 +1,35 @@
+const formFieldset = document.getElementById("form-fieldset");
+const formButton = document.getElementById("form-button");
+
+const apiURL = "https://127.0.0.1/label";
+
+const fetchLabel = async () => {
+    const response = await fetch(apiURL, {
+        method: "POST"
+    });
+    const data = await response.json();
+    data.label.forEach((d) => {
+        let p = document.createElement("p");
+
+        let checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("id", d);
+        checkBox.setAttribute("name", "label");
+        checkBox.setAttribute("value", d);
+
+        let label = document.createElement("label");
+        label.innerText = d;
+        label.setAttribute("for", d);
+
+        p.appendChild(checkBox);
+        p.appendChild(label);
+        formFieldset.appendChild(p);
+    });
+}
+
+const clickFormButton = (e) => {
+    e.preventDefault();
+
+}
+
+fetchLabel();
--- server.js
+++ server.js
@@ -1,10 +1,72 @@
 const express = require("express");
 const app=express();
+const https = require("https");
+const http = require('http');
+const fs= require("fs");
+const multer = require("multer");
 
-app.use(express.static("views"));
+require("dotenv").config(); //加载配置文件
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'commodityPic/')
+    },
+    filename: function (req, file, cb) {
+        let filename = req.body.fileName + "-" +req.body.label.join("-");
+        filename += "." + file.mimetype.split("/")[file.mimetype.split("/").length-1];
+        cb(null, filename)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+const options = {
+    key: fs.readFileSync('./cert/nlshop.com.cn.key'),
+    cert: fs.readFileSync('./cert/nlshop.com.cn_bundle.pem')
+};
+
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+    console.log('HTTP server listening on port 80');
+});
+
+
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(443, function () {
+    console.log("https server listening on port 443");
+    console.log("127.0.0.1:443");
+});
+
+
+// 将http 重定向到 https
+app.use((req, res, next) => {
+    if (req.protocol === 'http') {
+        const secureUrl = `https://${req.hostname}${req.url}`;
+        res.redirect(301, secureUrl);
+        console.log(secureUrl)
+    } else {
+        next();
+    }
+});
+
+
+app.use("/views", express.static("views"));
 app.use("/public",express.static("public"));
 app.use("/img",express.static("img"));
 
-app.listen(3000,() => {
-    console.log("127.0.0.1:3000")
+app.post("/label", (req, res) => {
+    const data = {
+        label: process.env.label.split(",")
+    };
+    res.json(data);
+})
+
+
+app.post("/upload",upload.single("commodityPic"), (req, res) => {
+    console.log(req.body);
+    // console.log(req.file);
+    const data = {
+        status: 200
+    };
+    res.json(data);
 });
